6be6e590e23d8205a7a5e70a90109a5e3b3f577f
Felipeco <fcorrea@condorlabs.io>
2020-05-14 18:01:26 -0500

    update base project

--- POCNeedle/SceneDelegate.swift
+++ POCNeedle/SceneDelegate.swift
@@ -18,6 +18,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+        let window = self.window ?? UIWindow(frame: UIScreen.main.bounds)
+        self.window = window
+
+        window.rootViewController = UIStoryboard(name: "Main", bundle: nil)
+            .instantiateViewController(identifier: "ViewController") as! ViewController
+        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
